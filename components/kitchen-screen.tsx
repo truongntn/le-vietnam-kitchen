cd58e565518b94f9463b5a6669f4d095bd6b5b2d
ntnt <ntnt@ntnts-MacBook-Pro.local>
2025-05-16 11:10:14 +0700

    update date time for checkin

--- components/kitchen-screen.tsx
+++ components/kitchen-screen.tsx
@@ -55,7 +55,14 @@ export default function KitchenScreen() {
 
   const formatTime = (timestamp: number) => {
     const date = new Date(timestamp);
-    return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    //return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+    return date.toLocaleString([], {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    });
   };
 
   return (
